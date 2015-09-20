dbb359f1a84fa8c8b97bf675980d59bd9cf0bb8a
bettehem <chris.mustola@gmail.com>
2015-09-20 21:07:29 +0300

    base system installation complete

--- installer.sh
+++ installer.sh
@@ -62,16 +62,21 @@ function get_using_usb(){
 #-------------------------------------------------------------------------------
 #Start of installer
 function installer(){
-	check_start
-	if [ "$PROGRESS" -gt 0 ]; then
-		continue_progress
+	if [ "$1" == 1 ]; then
+		goto_continue
 	else
+		check_start
+		if [ "$PROGRESS" -gt 0 ]; then
+			continue_progress
+		else
 		#Begins installation
 		keymap_view
+		fi
 	fi
 }
 
 #Checks if the .progress file exists and creates one if it doesn't
+#Checks if the .keyboard file exists and creates one if it doesn't
 #Also checks if .drive_details directory exists and creates one if it doesn't
 function check_start(){
 	if [ -e ".progress" ]; then
@@ -80,6 +85,10 @@ function check_start(){
 		touch .progress
 		echo "0" > .progress
 		get_progress
+	fi
+	
+	if [ ! -e ".keyboard" ]; then
+		touch .keyboard
 	fi
 	
 	if [ ! -d ".drive_details" ]; then
@@ -98,7 +107,7 @@ function continue_progress(){
 	select CONTINUE_SELECTION in "Yes" "No"; do
 		case $CONTINUE_SELECTION in
 			Yes ) goto_continue; break;;
-			No ) rm -r .progress; rm -r .drive_details; installer; break;;
+			No ) rm -r .progress; rm -r .drive_details; rm -r .keyboard; installer; break;;
 		esac
 	done
 }
@@ -114,6 +123,10 @@ function goto_continue(){
 		5) filesystems;;
 		6) mounting_partitions;;
 		7) install_base;;
+		8) fstab;;
+		9) timezone;;
+		10) chrooting;;
+		11) root_password;;
 	esac
 }
 #-------------------------------------------------------------------------------
@@ -143,12 +156,14 @@ function keymap_select(){
 	read -r SELECTED_KEYMAP
 	if [ "$SELECTED_KEYMAP" == "" ]; then 
 		loadkeys us
+		echo "us" > .keyboard
 	else
 		loadkeys $SELECTED_KEYMAP
+		echo "$SELECTED_KEYMAP" > .keyboard
 	fi
+	
 	clear
 	set_progress "2"
-	get_progress
 	disk_get
 }
 
@@ -332,8 +347,8 @@ function boot_filesystem(){
 		echo "The boot partiton will be labelled as $BOOT_LABEL."
 		echo "$BOOT_LABEL" > .drive_details/boot_label
 		
-		echo -e "Only f2fs and ext4 are supported with labels in this installation.\nIf you choose another filesystem, you have to label it manually."
-		echo "What filesystem do you want for your boot partition? (ext2 or ext4 recommended)"
+		echo -e "Only ext2 and ext4 are supported with labels in this installation.\nIf you choose another filesystem, you have to label it manually."
+		echo -e "What filesystem do you want for your boot partition?\n(ext2 or ext4 recommended)"
 		select FILESYSTEM_BOOT in "ext2" "ext4" "other"; do
 			case $FILESYSTEM_BOOT in
 				ext2) mkfs.ext2 -L $BOOT_LABEL "$SELECTED_DRIVE$BOOT_PARTITION"; echo "ext2" > .drive_details/boot_filesystem; echo ""; break;;
@@ -342,7 +357,7 @@ function boot_filesystem(){
 			esac
 		done
 	else
-		echo "What filesystem do you want for your boot partition? (ext2 or ext4 recommended)"
+		echo -e "What filesystem do you want for your boot partition?\n(ext2 or ext4 recommended)"
 		select FILESYSTEM_BOOT in "ext2" "ext4" "other"; do
 			case $FILESYSTEM_BOOT in
 				ext2) mkfs.ext2 "$SELECTED_DRIVE$BOOT_PARTITION"; echo "ext2" > .drive_details/boot_filesystem; echo ""; break;;
@@ -361,7 +376,7 @@ function root_filesystem(){
 		echo "$ROOT_LABEL" > .drive_details/root_label
 		
 		echo -e "Only f2fs and ext4 are supported with labels in this installation.\nIf you choose another filesystem, you have to label it manually."
-		echo "What filesystem do you want for your root partition? (f2fs recommended for usb installation)"
+		echo -e "What filesystem do you want for your root partition?\n(f2fs recommended for usb installation)"
 		select FILESYSTEM_ROOT in "f2fs" "ext4" "other"; do
 			case $FILESYSTEM_ROOT in
 				f2fs) mkfs.f2fs -l $ROOT_LABEL "$SELECTED_DRIVE$ROOT_PARTITION"; echo "f2fs" > .drive_details/root_filesystem; echo ""; break;;
@@ -388,7 +403,7 @@ function home_filesystem(){
 		echo "$HOME_LABEL" > .drive_details/home_label
 		
 		echo -e "Only f2fs and ext4 are supported with labels in this installation.\nIf you choose another filesystem, you have to label it manually."
-		echo "What filesystem do you want for your home partition? (f2fs recommended for usb installation)"
+		echo -e "What filesystem do you want for your home partition?\n(f2fs recommended for usb installation)"
 		select FILESYSTEM_HOME in "f2fs" "ext4" "other"; do
 			case $FILESYSTEM_HOME in
 				f2fs) mkfs.f2fs -l $HOME_LABEL "$SELECTED_DRIVE$HOME_PARTITION"; echo "f2fs" > .drive_details/home_filesystem; echo ""; break;;
@@ -431,8 +446,8 @@ function mounting_partitions(){
 function install_base(){
 	show_progress
 	pacstrap /mnt base base-devel
-	
 	echo "Base system installed"
+	
 	set_progress "8"
 	clear
 	fstab
@@ -440,8 +455,342 @@ function install_base(){
 
 #Part 8
 function fstab(){
+	show_progress
 	echo "Generating fstab..."
+	genfstab -L /mnt > /mnt/etc/fstab
+	echo "Generated fstab."
 	
+	set_progress "9"
+	clear
+	timezone
+}
+
+#Part 9
+function timezone(){
+	show_progress
+	echo "Listing timezones. press \"q\" when you are done."
+	sleep 2
+	timedatectl list-timezones
+	clear
+	printf "Enter your timezone (For example \"Europe/Helsinki\"): "
+	read -r TIMEZONE
+	echo "Setting timezone to $TIMEZONE"
+	ln -s /mnt/usr/share/zoneinfo/$TIMEZONE /mnt/etc/localtime
+	
+	set_progress "10"
+	clear
+	chrooting
+}
+
+#Part 10
+function chrooting(){
+	show_progress
+	echo "Copying installer files in to /mnt/root/ArchLinuxInstaller"
+	mkdir /mnt/root/ArchLinuxInstaller
+	cp -a * /mnt/root/ArchLinuxInstaller/.
+	cp -a .* /mnt/root/ArchLinuxInstaller/.
+	
+	set_progress "11"
+	
+	echo "Chrooting in to /mnt and launching installer.."
+	arch-chroot /mnt ./root/ArchLinuxInstaller/installer.sh "1"
+	clear
+	finish-install
+}
+
+#Continuing from here, the installer will be executed in chroot
+#Part 11
+function root_password(){
+	show_progress
+	printf "Do you want to set a password for the root user? This is highly recommended[y/n]: "
+	read -r ROOT_PASSWORD
+	if [ "$ROOT_PASSWORD" == "y" ]; then
+		passwd
+	elif [ "$ROOT_PASSWORD" == "Y" ]; then
+		passwd
+	elif [ "$ROOT_PASSWORD" == "n" ]; then
+		;
+	elif [ "$ROOT_PASSWORD" == "N" ]; then
+		;
+	else
+		root_password
+	fi
+	
+	set_progress "12"
+	clear
+	locale_setting
+}
+
+#Part 12
+function locale_setting(){
+	show_progress
+	printf "Next you have to use nano to uncomment all the locales you want to be available.\nWhen you are done, press CTRL+O (the letter O) to save.\nThen press CTRL+X to exit the editor.\nPress enter to continue."
+	read -r
+	nano /etc/locale.gen
+	locale-gen
+	echo "listing locales. press \"q\" when you know which one you want to use."
+	localectl list-locales | less
+	printf "Enter the locale you want to use as it is dispalyed.\nFor example \"en_US.utf8\": "
+	read -r USER_LOCALE
+	localectl set-locale LOCALE=$USER_LOCALE
+	
+	set_progress "13"
+	clear
+	set_keyboard
+}
+
+#Part 13
+function set_keyboard(){
+	show_progress
+	echo "Setting keyboard layout to $(cat .keyboard)"
+	localectl set-keymap --no-convert "$(cat .keyboard)"
+	localectl set-x11-keymap --no-convert "$(cat .keyboard)"
+	
+	set_progress "14"
+	clear
+	set_hostname
+}
+
+#Part 14
+function set_hostname(){
+	show_progress
+	printf "Enter a hostname for your computer(Name of your computer): "
+	read -r COMPUTER_HOSTNAME
+	echo "$COMPUTER_HOSTNAME" > /etc/hostname
+	
+	set_progress "15"
+	clear
+	set_multilib
+}
+
+#Part 15
+function set_multilib(){
+	show_progress
+	echo "checking if multilib is required..."
+	if [ uname -a == "x86_64" ]; then
+		echo "Enabling multilib..."
+		if [ grep -q "#\[multilib\]" /etc/pacman.conf ]; then
+        	sed -i '/\[multilib\]/{ s/^#//; n; s/^#//; }' /etc/pacman.conf
+		elif [ ! grep -q "\[multilib\]" /etc/pacman.conf ]; then
+        	printf "[multilib]\nInclude = /etc/pacman.d/mirrorlist" \ 
+			>> /etc/pacman.conf
+		fi
+	fi
+	
+	set_progress "16"
+	clear
+	set_yaourt
+}
+
+#Part 16
+function set_yaourt(){
+	show_progress
+	echo "Enabling yaourt..."
+	if [ grep -q "#\[archlinuxfr\]" /etc/pacman.conf ]; then
+        sed -i '/\[archlinuxfr\]/{ s/^#//; n; s/^#//; }' /etc/pacman.conf
+	elif [ ! grep -q "\[archlinuxfr\]" /etc/pacman.conf ]; then
+        printf "[archlinuxfr]\nSigLevel = Never\nServer = http://repo.archlinux.fr/\$arch\n" \ 
+		>> /etc/pacman.conf
+	fi
+	pacman -Sy yaourt --noconfirm --needed
+	set_progress "17"
+	clear
+	grub_setup
+}
+
+#Part 17
+function grub_setup(){
+	show_progress
+	printf "Do you want to install grub?(Recommended)[y/n]: "
+	read -r GRUB_INSTALL
+	if [ "$GRUB_INSTALL" == "y" ]; then
+		pacman -Sy grub-bios --noconfirm --needed
+	elif [ "$GRUB_INSTALL" == "Y" ]; then
+		pacman -Sy grub-bios --noconfirm --needed
+	elif [ "$GRUB_INSTALL" == "n" ]; then
+		pacman -Sy grub-bios --noconfirm --needed
+	elif [ "$GRUB_INSTALL" == "N" ]; then
+		pacman -Sy grub-bios --noconfirm --needed
+	else
+		grub_setup
+	fi
+	
+	set_progress "18"
+	clear
+	check_multiboot
+}
+
+
+
+#Part 18
+function check_multiboot(){
+	show_progress
+	get_using_usb
+	if [ "$USING_USB" == "1" ]; then
+		;
+	else
+		printf "Do you have another os installed alongside arch?[y/n]: "
+		read -r MULTI_OS
+		if [ $MULTI_OS == "y" ]; then
+			pacman -Sy os-prober --noconfirm --needed
+			os-prober
+		elif [ $MULTI_OS == "Y" ]; then
+			pacman -Sy os-prober --noconfirm --needed
+			os-prober
+		elif [ $MULTI_OS == "n" ]; then
+			;
+		elif [ $MULTI_OS == "N" ]; then
+			;
+		else
+			check_multiboot
+		fi
+	fi
+	
+	set_progress "19"
+	clear
+	install_grub
+}
+
+#Part 19
+function install_grub(){
+	show_progress
+	
+	get_drive
+	grub-install $SELECTED_DRIVE
+	
+	get_using_usb
+	
+	if [ "$USING_USB" == "1" ]; then
+		pacman -Sy f2fs-tools --noconfirm --needed
+	fi
+	
+	mkinitcpio -p linux
+	if [ "$USING_USB" == "1" ]; then
+		if [ grep -q "#GRUB_DISABLE_LINUX_UUID=\"true\"" /etc/default/grub ]; then
+        	sed -i '/GRUB_DISABLE_LINUX_UUID=\"true\"/{ s/^#//;}' /etc/default/grub
+		fi
+	fi
+	grub-mkconfig -o /boot/grub/grub.cfg
+	if [ "$USING_USB" == "1" ]; then
+		sed -i "s/$SELECTED_DRIVE$ROOT_PARTITION/g" /boot/grub/grub.cfg
+	fi
+	
+	set_progress "20"
+	clear
+	network
+}
+
+
+
+#Part 20
+function network(){
+	show_progress
+	
+	connman_install
+	network_manager_install
+	systemctl enable dhcpcd
+	
+	set_progress "21"
+	clear
+	normal_user
+}
+
+#These are for Part 20
+#################################################################################
+function connman_install(){
+	printf "Do you want to install connman?(Recommended for easy usage)[y/n]: "
+	read -r CONNMAN
+	if [ "$CONNMAN" == "y" ]; then
+		pacman -S connman --noconfirm --needed
+		systemctl enable connman
+		printf "Enable wifi?[y/n]: "
+		read -r ENABLE_WIFI
+		if [ "$ENABLE_WIFI" == "y" ]; then
+			connmanctl enable wifi
+		elif [ "$ENABLE_WIFI" == "Y" ]; then
+			connmanctl enable wifi
+		elif [ "$ENABLE_WIFI" == "n" ]; then
+			;
+		elif [ "$ENABLE_WIFI" == "N" ]; then
+			;
+		else
+			connman_install
+		fi
+	elif [ "$CONNMAN" == "Y" ]; then
+		pacman -S connman --noconfirm --needed
+		systemctl enable connman
+	elif [ "$CONNMAN" == "n" ]; then
+		;
+	elif [ "$CONNMAN" == "N" ]; then
+		;
+	else
+		connman_install
+	fi
+}
+
+function networkmanager_install(){
+	printf "Do you want to install networkmanager?(Recommended for easy usage)[y/n]: "
+	read -r NETWORKMANAGER
+	if [ "$NETWORKMANAGER" == "y" ]; then
+		pacman -S networkmanager --noconfirm --needed
+		systemctl enable networkmanager
+	elif [ "$NETWORKMANAGER" == "Y" ]; then
+		pacman -S networkmanager --noconfirm --needed
+		systemctl enable networkmanager
+	elif [ "$NETWORKMANAGER" == "n" ]; then
+		;
+	elif [ "$NETWORKMANAGER" == "N" ]; then
+		;
+	else
+		networkmanager_install
+	fi
+}
+#################################################################################
+
+#Part 21
+function normal_user(){
+	show_progress
+
+	printf "Add a normal user?(Recommended)[y/n]: "
+	read -r ADD_USER
+	if [ "$ADD_USER" == "y" ]; then
+		printf "An username can onky contain lower-case letters and can not contain spaces.\nEnter username: "
+		read -r USERNAME
+		useradd -m -g users -G storage,power,wheel -s /bin/bash $USERNAME
+		passwd $USERNAME
+	elif [ "$ADD_USER" == "Y" ]; then
+		printf "An username can onky contain lower-case letters and can not contain spaces.\nEnter username: "
+		read -r USERNAME
+		useradd -m -g users -G storage,power,wheel -s /bin/bash $USERNAME
+		passwd $USERNAME
+	elif [ "$ADD_USER" == "n" ]; then
+		;
+	elif [ "$ADD_USER" == "N" ]; then
+		;
+	else
+		normal_user
+	fi
+	
+	set_progress "22"
+	clear
+	exit
+}
+
+#Part 22
+function finish_install(){
+	show_progress
+	echo "Unmounting partitions..."
+	if [ -f ".drive_details/boot" ]; then
+		umount /mnt/boot
+	fi
+	if [ -f ".drive_details/home" ]; then
+		umount /mnt/home
+	fi
+	umount /mnt
+	
+	echo -e "Done. Press enter to shut down the computer.\nThen remove the installation media and enjoy your new arch linux system!"
+	read
+	shutdown -h now
 }
 #-------------------------------------------------------------------------------
 
@@ -449,4 +798,4 @@ function fstab(){
 
 
 #Used to launch the script
-installer
+installer "$1"
