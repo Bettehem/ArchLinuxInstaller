7946a28dc271cfab3ecf8e3bb9f1ad0055777676
bettehem <chris.mustola@gmail.com>
2015-09-20 15:43:45 +0300

    Added filesystem creation and base system installation

--- installer.sh
+++ installer.sh
@@ -14,7 +14,7 @@ echo -e "Copyright 2015 Chris Mustola\n\nArch Linux installer tool\n------------
 #Initialize variables
 PROGRESS=""
 SELECTED_DRIVE=""
-TOTAL_PROGRESS=2
+TOTAL_PROGRESS=7
 #-------------------------------------------------------------------------------
 
 
@@ -88,7 +88,7 @@ function continue_progress(){
 	select CONTINUE_SELECTION in "Yes" "No"; do
 		case $CONTINUE_SELECTION in
 			Yes ) goto_continue; break;;
-			No ) echo "0" > .progress; installer; break;;
+			No ) rm .progress; if [ -d ".drive_details" ]; then; rm -r .drive_details installer; fi; break;;
 		esac
 	done
 }
@@ -156,6 +156,7 @@ function disk_partition(){
 	show_progress
 	set_drive "$SELECTED_DRIVE"
 	echo "Do you want to partition $SELECTED_DRIVE?"
+	echo "WARNING! IT\'S NOT RECOMMENDED TO CREATE A SWAP PARTITION IF YOU ARE INSTALLING ON TO A USB DRIVE!"
 	select DRIVE_PARTITION in "Yes" "No"; do
 		case $DRIVE_PARTITION in
 			Yes) cfdisk $SELECTED_DRIVE; set_progress "4"; clear; disk_partitioned; break;;
@@ -167,14 +168,25 @@ function disk_partition(){
 #Part 4
 function disk_partitioned(){
 	show_progress
+	
 	boot_created
+	root_created
+	home_created
+	swap_created
+	
+	set_progress "5"
+	clear
+	filesystems
 }
-
+#These are for Part 4
+#################################################################################
 function boot_created(){
-	printf "Did you create a separate boot partition?[y/n]"
+	printf "Did you create a boot partition?[y/n]: "
 	read -r CREATED_BOOT
 	if [ "$CREATED_BOOT" == "y" ]; then
-		printf "Enter partition number (for example if your boot partition is ): "
+		printf "Enter partition number\n(for example if your boot partition is /dev/sda1 enter \"1\"): "
+		read -r BOOT_PARTITION
+		echo "$SELECTED_DRIVE/$BOOT_PARTITION" > .drive_details/boot
 	elif [ "$CREATED_BOOT" == "Y" ]; then
 		echo ""
 	elif [ "$CREATED_BOOT" == "n" ]; then
@@ -185,6 +197,207 @@ function boot_created(){
 		clear
 		boot_created
 	fi
+}
+
+function root_created(){
+	printf "Did you create a root partition?[y/n]: "
+	read -r CREATED_ROOT
+	if [ "$CREATED_ROOT" == "y" ]; then
+		printf "Enter partition number\n(for example if your root partition is /dev/sda2 enter \"2\"): "
+		read -r ROOT_PARTITION
+		echo "$SELECTED_DRIVE/$ROOT_PARTITION" > .drive_details/root
+	elif [ "$CREATED_ROOT" == "Y" ]; then
+		echo ""
+	elif [ "$CREATED_ROOT" == "n" ]; then
+		echo ""
+	elif [ "$CREATED_ROOT" == "N" ]; then
+		echo ""
+	else
+		root_created
+	fi
+}
+
+function home_created(){
+	printf "Did you create a root partition?[y/n]: "
+	read -r CREATED_HOME
+	if [ "$CREATED_HOME" == "y" ]; then
+		printf "Enter partition number\n(for example if your home partition is /dev/sda3 enter \"3\"): "
+		read -r HOME_PARTITION
+		echo "$SELECTED_DRIVE/$HOME_PARTITION" > .drive_details/home
+	elif [ "$CREATED_HOME" == "Y" ]; then
+		echo ""
+	elif [ "$CREATED_HOME" == "n" ]; then
+		echo ""
+	elif [ "$CREATED_HOME" == "N" ]; then
+		echo ""
+	else
+		home_created
+	fi
+}
+
+function swap_created(){
+	printf "Did you create a root partition?[y/n]: "
+	read -r CREATED_SWAP
+	if [ "$CREATED_SWAP" == "y" ]; then
+		printf "Enter partition number\n(for example if your swap partition is /dev/sda4 enter \"4\"): "
+		read -r SWAP_PARTITION
+		echo "$SELECTED_DRIVE/$SWAP_PARTITION" > .drive_details/swap
+	elif [ "$CREATED_SWAP" == "Y" ]; then
+		echo ""
+	elif [ "$CREATED_SWAP" == "n" ]; then
+		echo ""
+	elif [ "$CREATED_SWAP" == "N" ]; then
+		echo ""
+	else
+		swap_created
+	fi
+}
+#################################################################################
+
+#Part 5
+function filesystems(){
+	show_progress
+	
+	using_usb
+	if [ -f ".drive_details/boot" ]; then
+		boot_filesystem
+	fi
+	if [ -f ".drive_details/root" ]; then
+		root_filesystem
+	fi
+	if [ -f ".drive_details/home" ]; then
+		home_filesystem
+	fi
+	mkswap "$SELECTED_DRIVE/$SWAP_PARTITION"
+	
+	set_progress "6"
+	clear
+	mounting_partitions
+}
+#These are for Part 5
+#################################################################################
+function using_usb(){
+	printf "Are you installing your system on to a usb stick?[y/n] "
+	read -r USING_USB
+	if [ "$USING_USB" == "y" ]; then
+		echo "1" > .drive_details/usb
+	elif [ "$USING_USB" == "Y" ]; then
+		echo ""
+	elif [ "$USING_USB" == "n" ]; then
+		echo ""
+	elif [ "$USING_USB" == "N" ]; then
+		echo ""
+	else
+		using_usb
+	fi
+}
+
+function boot_filesystem(){
+	if [ "$USING_USB" == "1" ]; then
+		printf "Enter a label for your boot partition.\nThis is recommended to ensure compatibility between machines(for example \"arch_usb_boot\"): "
+		read -r BOOT_LABEL
+		echo "The boot partiton will be labelled as $BOOT_LABEL."
+		echo "$BOOT_LABEL" > .drive_details/boot_label
+		
+		echo -e "Only f2fs and ext4 are supported with labels in this installation.\nIf you choose another filesystem, you have to label it manually."
+		echo "What filesystem do you want for your root partition? (f2fs recommended for usb installation)"
+		select FILESYSTEM_BOOT in "f2fs" "ext4" "other"; do
+			case $FILESYSTEM_BOOT in
+				f2fs) mkfs.f2fs -L $BOOT_LABEL "$SELECTED_DRIVE/$BOOT_PARTITION"; echo "f2fs" > .drive_details/boot_filesystem; break;;
+				ext4) mkfs.ext4 -L $BOOT_LABEL "$SELECTED_DRIVE/$BOOT_PARTITION"; echo "ext4" > .drive_details/boot_filesystem; break;;
+				other) printf "Enter filesystem to be used: "; read -r CUSTOM_BOOT_FILESYSTEM; echo "$CUSTOM_BOOT_FILESYSTEM" > .drive_details/custom_boot_filesystem; mkfs."$CUSTOM_BOOT_FILESYSTEM" "$SELECTED_DRIVE/$BOOT_PARTITION"; break;;
+			esac
+		done
+	else
+		echo "What filesystem do you want for your boot partition? (ext2 or ext4 recommended)"
+		select FILESYSTEM_BOOT in "ext2" "ext4" "other"; do
+			case $FILESYSTEM_BOOT in
+				ext2) mkfs.ext2 "$SELECTED_DRIVE/$BOOT_PARTITION"; echo "ext2" > .drive_details/boot_filesystem; break;;
+				ext4) mkfs.ext4 "$SELECTED_DRIVE/$BOOT_PARTITION"; echo "ext4" > .drive_details/boot_filesystem; break;;
+				other) printf "Enter filesystem to be used: "; read -r CUSTOM_BOOT_FILESYSTEM; echo "$CUSTOM_BOOT_FILESYSTEM" > .drive_details/custom_boot_filesystem; mkfs."$CUSTOM_BOOT_FILESYSTEM" "$SELECTED_DRIVE/$BOOT_PARTITION"; break;;
+			esac
+		done
+	fi
+}
+
+function root_filesystem(){
+	if [ "$USING_USB" == "1" ]; then
+		printf "Enter a label for your root partition.\nThis is recommended to ensure compatibility between machines(for example \"arch_usb_root\"): "
+		read -r ROOT_LABEL
+		echo "The root partiton will be labelled as $ROOT_LABEL."
+		echo "$ROOT_LABEL" > .drive_details/root_label
+		
+		echo -e "Only f2fs and ext4 are supported with labels in this installation.\nIf you choose another filesystem, you have to label it manually."
+		echo "What filesystem do you want for your root partition? (f2fs recommended for usb installation)"
+		select FILESYSTEM_ROOT in "f2fs" "ext4" "other"; do
+			case $FILESYSTEM_ROOT in
+				f2fs) mkfs.f2fs -L $ROOT_LABEL "$SELECTED_DRIVE/$ROOT_PARTITION"; echo "f2fs" > .drive_details/root_filesystem; break;;
+				ext4) mkfs.ext4 -L $ROOT_LABEL "$SELECTED_DRIVE/$ROOT_PARTITION"; echo "ext4" > .drive_details/root_filesystem; break;;
+				other) printf "Enter filesystem to be used: "; read -r CUSTOM_ROOT_FILESYSTEM; echo "$CUSTOM_ROOT_FILESYSTEM" > .drive_details/custom_root_filesystem; mkfs."$CUSTOM_ROOT_FILESYSTEM" "$SELECTED_DRIVE/$ROOT_PARTITION"; break;;
+			esac
+		done
+	else
+		echo "What filesystem do you want for your root partition? (ext4 recommended)"
+		select FILESYSTEM_ROOT in "ext4" "other"; do
+			case $FILESYSTEM_ROOT in
+				ext4) mkfs.ext2 "$SELECTED_DRIVE/$ROOT_PARTITION"; echo "ext4" > .drive_details/root_filesystem; break;;
+				other) printf "Enter filesystem to be used: "; read -r CUSTOM_ROOT_FILESYSTEM; echo "$CUSTOM_ROOT_FILESYSTEM" > .drive_details/custom_root_filesystem; mkfs."$CUSTOM_ROOT_FILESYSTEM" "$SELECTED_DRIVE/$ROOT_PARTITION"; break;;
+			esac
+		done
+	fi
+}
+
+function home_filesystem(){
+	if [ "$USING_USB" == "1" ]; then
+		printf "Enter a label for your home partition.\nThis is recommended to ensure compatibility between machines(for example \"arch_usb_home\"): "
+		read -r HOME_LABEL
+		echo "The home partiton will be labelled as $HOME_LABEL."
+		echo "$HOME_LABEL" > .drive_details/home_label
+		
+		echo -e "Only f2fs and ext4 are supported with labels in this installation.\nIf you choose another filesystem, you have to label it manually."
+		echo "What filesystem do you want for your home partition? (f2fs recommended for usb installation)"
+		select FILESYSTEM_HOME in "f2fs" "ext4" "other"; do
+			case $FILESYSTEM_HOME in
+				f2fs) mkfs.f2fs -L $HOME_LABEL "$SELECTED_DRIVE/$HOME_PARTITION"; echo "f2fs" > .drive_details/home_filesystem; break;;
+				ext4) mkfs.ext4 -L $HOME_LABEL "$SELECTED_DRIVE/$HOME_PARTITION"; echo "ext4" > .drive_details/home_filesystem; break;;
+				other) printf "Enter filesystem to be used: "; read -r CUSTOM_HOME_FILESYSTEM; echo "$CUSTOM_HOME_FILESYSTEM" > .drive_details/custom_home_filesystem; mkfs."$CUSTOM_HOME_FILESYSTEM" "$SELECTED_DRIVE/$HOME_PARTITION"; break;;
+			esac
+		done
+	else
+		echo "What filesystem do you want for your home partition? (ext4 recommended)"
+		select FILESYSTEM_HOME in "ext4" "other"; do
+			case $FILESYSTEM_HOME in
+				ext4) mkfs.ext2 "$SELECTED_DRIVE/$HOME_PARTITION"; echo "ext4" > .drive_details/home_filesystem; break;;
+				other) printf "Enter filesystem to be used: "; read -r CUSTOM_HOME_FILESYSTEM; echo "$CUSTOM_HOME_FILESYSTEM" > .drive_details/custom_home_filesystem; mkfs."$CUSTOM_HOME_FILESYSTEM" "$SELECTED_DRIVE/$HOME_PARTITION"; break;;
+			esac
+		done
+	fi
+}
+#################################################################################
+
+#Part 6
+function mounting_partitions(){
+	show_progress
+	echo "Mounting partitions..."
+	mount "$SELECTED_DRIVE/$ROOT_PARTITION" /mnt
+	if [ -f ".drive_details/boot" ]; then
+		mkdir /mnt/boot
+		mount "$SELECTED_DRIVE/$ROOT_PARTITION" /mnt/boot
+	fi
+	if [ -f ".drive_details/home" ]; then
+		mkdir /mnt/home
+		mount "$SELECTED_DRIVE/$HOME_PARTITION" /mnt/home
+	fi
+	
+	set_progress "7"
+	clear
+	install_base
+}
+
+#Part 7
+function install_base(){
+	show_progress
+	pacstrap /mnt base base-devel
 }
 #-------------------------------------------------------------------------------
 
